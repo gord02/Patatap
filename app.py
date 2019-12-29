4c0c3d92c3ef7e59be697d44ad73b869a270c1fc
gord02 <53834349+gord02@users.noreply.github.com>
2019-12-29 18:01:20 -0500

    Modify app.py

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template, make_response, request
+from flask import Flask, make_response, render_template, request
 
 # intilalizies flask app(server)
 app = Flask(__name__)
 
 # creates one (index) route
-@app.route("/", methods=['GET'])
+@app.route("/", methods=['GET', 'POST'])
 def index():
+	# checks if request was GET request, if so makes respones of error type code(number)
 	if request.method == 'GET':
 		return make_response(render_template('index.html'), 200)
-	else:
-		app.logger.error("Request of type: " + request.method + " isnt handled by this route.")
+
+	# else not needed cause return is skipped, makes a log of info and return reposne to user
+	app.logger.error("Request of type: " + request.method + " isnt handled by this route.")
+	return make_response("Error", 400)
 
 # a route to catch all other undefined routes
 @app.route("/<path:path>", methods=['GET'])
